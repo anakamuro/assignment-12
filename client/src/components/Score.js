7e261a4a799013984d4dc902a097d0afc5567824
Akihiko Nakamura <akihikonakamura@akihikos-mini.myfiosgateway.com>
2022-12-28 20:37:48 -0500

    commit

--- client/src/components/Score.js
+++ client/src/components/Score.js
@@ -1,30 +1,91 @@
 import React, { useState, useEffect } from 'react';
-import { RadialBarChart, RadialBar } from 'recharts';
-  
-  
+import Chart from 'react-apexcharts';
+
+
 function Score() {
-    const [post, setPost ] = useState({});
-    
-  
-    useEffect(()=> {
-      fetchPosts()
-    }, []);
-  
-    const fetchPosts = () => {
-      let x = 12
-      fetch(`http://localhost:3001/user/${x}`)
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response.data)
-          setPost(response.data)
-        })
+  const [post, setPost] = useState({});
+
+
+  useEffect(() => {
+    fetchPosts()
+  }, []);
+
+  const fetchPosts = () => {
+    let x = 12
+    fetch(`http://localhost:3001/user/${x}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.data)
+        setPost(response.data)
+      })
+  }
+
+  let todayScorePercent = post.todayScore * 100
+  let options = {
+    labels: [`${todayScorePercent}% of your goal`],
+
+    plotOptions: {
+
+      radialBar: {
+
+        hollow: {
+          size: '70%',
+
+        }
+      }
     }
-  
-return (
-  <RadialBarChart className="score" width={500} height={500} data={post.todayScore}>
-    <RadialBar minAngle={15} dataKey="todayScore" />
-  </RadialBarChart>
-);
+
+  }
+
+  return (
+    <div id="chart">
+      <h4 className="score2">Score</h4>
+      <Chart className="score" options={{
+        labels: [`${todayScorePercent}% of your goal`],
+
+        plotOptions: {
+
+          radialBar: {
+            dataLabels: {
+              name: {
+                color: '#000000'
+              },
+              value: {
+                fontSize: '0px',
+
+              },
+
+            },
+
+            hollow: {
+              size: '70%',
+
+            }
+          }
+        },
+        colors: ['#ff0000'],
+
+
+
+      }} series={[todayScorePercent]} type="radialBar" height={300} />
+    </div>
+  )
 }
-  
+
+// return null
+/*
+<RadialBarChart
+cx="50%" cy="50%" innerRadius="20%" outerRadius="80%" barSize={20} 
+className="score" width={500} height={500} data={dataArray}>
+  <h1>Hello</h1>
+  <RadialBar minAngle={15}
+    // label={{ fill: "#fff" }}
+    stroke="blue" 
+    
+    dataKey="todayScore" />
+</RadialBarChart>
+ 
+);*/
+
+
 export default Score;
--- client/src/components/Types.js
+++ client/src/components/Types.js
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
 
 
-    function Types() {
-    const [post, setPost] = useState({});
-  
-  
-    useEffect(() => {
-      fetchPosts()
-    }, []);
-  
-    const fetchPosts = () => {
-      fetch(`http://localhost:3001/user/12/performance`)
-        .then((response) => response.json())
-        .then((response) => {
-          console.log("types response ", response.data)
-          setPost(response.data)
-        })
-    }
- {/*
+function Types() {
+  const [post, setPost] = useState({});
+
+
+  useEffect(() => {
+    fetchPosts()
+  }, []);
+
+  const fetchPosts = () => {
+    fetch(`http://localhost:3001/user/12/performance`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("types response ", response.data)
+        setPost(response.data)
+      })
+  }
+  {/*
       <ResponsiveContainer className="types" width="100%" height="100%">
         <RadarChart cx="50%" cy="50%" outerRadius="80%" data={post.kinds}>
           <PolarGrid />
@@ -29,34 +29,48 @@ import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from 'r
     </ResponsiveContainer>*/}
 
 
+  // [{
+  //   "value": 80,
+  //   "kind": 1
+  // }, {
+  //   "value": 80,
+  //   "kind": 2
+  // },]
 
-   if(post && post.kind){
-    let arr=post.data
-    arr=arr.map(item=>{
-                                               
-      return {value:item.value,kind:post.kind[item.kind]}
+  // [{
+  //   "value": 80,
+  //   "kind": "cardion"
+  // }, {
+  //   "value": 80,
+  //   "kind": "energy"
+  // },]
+
+  if (post && post.kind) {
+
+    let arr = post.data.map(item => {
+      return { value: item.value, kind: post.kind[item.kind] }
     })
-    console.log("arr",arr)
+    console.log("arr", arr)
     return (
 
       <RadarChart className="types" outerRadius={90} width={350} height={300} data={arr}>
-      <PolarGrid />
-      <PolarAngleAxis dataKey="kind" />
-      <PolarRadiusAxis angle={30} domain={[0, 150]} />
-      <Radar
-        name="value"
-        dataKey="value"
-        stroke="rgba(255, 1, 1, 0.7)"
-        fill="rgba(255, 1, 1, 0.7)"
-        fillOpacity={0.6}
-      />
-    
-      
-    </RadarChart>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="kind" />
+        <PolarRadiusAxis angle={30} domain={[0, 150]} />
+        <Radar
+          name="value"
+          dataKey="value"
+          stroke="rgba(255, 1, 1, 0.7)"
+          fill="rgba(255, 1, 1, 0.7)"
+          fillOpacity={0.6}
+        />
+
+
+      </RadarChart>
     )
 
-   }
   }
-    
+}
+
 
 export default Types;
